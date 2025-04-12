351c559d8f1f5f413b319ad058fdfdd18af70778
Ryan Enns <ryanennns@gmail.com>
2025-04-12 10:21:10 -0400

    timing changes

--- routes/console.php
+++ routes/console.php
@@ -2,6 +2,6 @@
 
 use Illuminate\Support\Facades\Schedule;
 
-Schedule::command('elo:singleplayer')->dailyAt('10:20');
-Schedule::command('elo:teams')->dailyAt('10:40');
-Schedule::command('snapshot:generate')->dailyAt('11:00');
+Schedule::command('elo:singleplayer')->dailyAt('10:25');
+Schedule::command('elo:teams')->dailyAt('10:45');
+Schedule::command('snapshot:generate')->dailyAt('11:05');
