b8629a28695eef5aa9909f1e382ae6962b238d3d
Ryan Enns <ryanennns@gmail.com>
2025-04-15 13:17:30 -0400

    fix team flags

--- resources/js/Pages/PlayerLeaderboard.vue
+++ resources/js/Pages/PlayerLeaderboard.vue
@@ -114,24 +114,19 @@
                                         :title="countryMap[player.country_code]"
                                     >
                                 </div>
-                                <div v-else-if="selectedMode.value === 'team'" class="flex">
-                                    <div v-if="!player.country_code">
-                                        <img
-                                            class="px-1"
-                                            :src="`https://flagcdn.com/32x24/${player.player_a.country_code}.png`"
-                                            :alt="player.country_code"
-                                            :title="countryMap[player.country_code]"
-                                        >
-                                        <img
-                                            class="px-1"
-                                            :src="`https://flagcdn.com/32x24/${player.player_b.country_code}.png`"
-                                            :alt="player.country_code"
-                                            :title="countryMap[player.country_code]"
-                                        >
-                                    </div>
-                                    <div v-else>
-                                        🏴 / 🏴
-                                    </div>
+                                <div v-else class="flex">
+                                    <img
+                                        class="px-1"
+                                        :src="`https://flagcdn.com/32x24/${player.player_a.country_code}.png`"
+                                        :alt="player.country_code"
+                                        :title="countryMap[player.country_code]"
+                                    >
+                                    <img
+                                        class="px-1"
+                                        :src="`https://flagcdn.com/32x24/${player.player_b.country_code}.png`"
+                                        :alt="player.country_code"
+                                        :title="countryMap[player.country_code]"
+                                    >
                                 </div>
                             </div>
                         </td>
