dcd37e10f643d3d451b29971d3593af6d3eb3198
ryan <ryanennns@gmail.com>
2025-04-19 14:02:08 -0400

    formatting

--- resources/js/Components/Footer.vue
+++ resources/js/Components/Footer.vue
@@ -4,9 +4,7 @@
         <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
             <div class="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-8">
                 <div class="mb-6 md:mb-0">
-                    <h3 class="text-lg font-semibold mb-4">
-                        About GeoGuessr Stats
-                    </h3>
+                    <h3 class="text-lg font-semibold mb-4">About GeoScrapr</h3>
                     <p class="text-gray-300 text-sm">
                         Every day at midnight Eastern Time, this application
                         scrapes the official GeoGuessr APIs for updated player
@@ -54,52 +52,44 @@
                 <div class="mb-6 md:mb-0">
                     <h3 class="text-lg font-semibold mb-4">Resources</h3>
                     <ul class="space-y-2">
-                        <li>
+                        <li class="mb-1">
                             <a
                                 href="https://geoguessr.com"
                                 class="text-gray-300 hover:text-white text-sm"
-                                >Official GeoGuessr Site</a
                             >
+                                Official GeoGuessr Site
+                            </a>
                         </li>
-                        <li>
+                        <li class="mb-1">
                             <a
                                 href="https://github.com/ryanennns/geoscrapr"
                                 class="text-gray-300 hover:text-white text-sm"
-                                >GitHub Repository</a
                             >
+                                GitHub Repository
+                            </a>
                         </li>
-                        <li>
+                        <li class="mb-1">
                             <a
                                 href="https://en.wikipedia.org/wiki/Elo_rating_system"
                                 class="text-gray-300 hover:text-white text-sm"
-                                >Rating System, Explained</a
                             >
+                                Rating System, Explained
+                            </a>
                         </li>
-                        <li>
+                        <li class="mb-1">
                             <a
                                 href="https://www.plonkit.net/beginners-guide"
                                 class="text-gray-300 hover:text-white text-sm"
-                                >Learn GeoGuessr</a
                             >
+                                Learn GeoGuessr
+                            </a>
                         </li>
                     </ul>
                 </div>
 
                 <div>
-                    <h3 class="text-lg font-semibold mb-4">Connect</h3>
-                    <div class="flex space-x-4 mb-4">
-                        <a href="#" class="text-gray-300 hover:text-white">
-                            <span class="sr-only">Twitter</span>
-                            <svg
-                                class="h-6 w-6"
-                                fill="currentColor"
-                                viewBox="0 0 24 24"
-                            >
-                                <path
-                                    d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84"
-                                ></path>
-                            </svg>
-                        </a>
+                    <div class="flex space-x-4">
+                        <h3 class="text-lg font-semibold mb-4">Connect</h3>
                         <a
                             href="https://github.com/ryanennns"
                             class="text-gray-300 hover:text-white"
@@ -144,22 +134,25 @@
                             <li>
                                 <a
                                     class="text-xs text-gray-400 hover:cursor-default"
-                                    >I don't have any of these lol --></a
                                 >
+                                    I don't have any of these lol -->
+                                </a>
                             </li>
                             <li>
                                 <a
                                     href="#"
                                     class="text-xs text-gray-400 hover:text-gray-300"
-                                    ><s>Privacy Policy</s></a
                                 >
+                                    <s>Privacy Policy</s>
+                                </a>
                             </li>
                             <li>
                                 <a
                                     href="#"
                                     class="text-xs text-gray-400 hover:text-gray-300"
-                                    ><s>Terms of Service</s></a
                                 >
+                                    <s>Terms of Service</s>
+                                </a>
                             </li>
                             <li>
                                 <a
