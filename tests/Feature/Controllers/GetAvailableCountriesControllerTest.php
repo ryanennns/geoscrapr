f70012965764c57817c08d7b4d059acafb41339d
ryan <ryanennns@gmail.com>
2025-04-23 15:48:51 -0400

    test GetAvailableCountriesController

---
+++ tests/Feature/Controllers/GetAvailableCountriesControllerTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Feature\Controllers;
+
+use App\Models\Player;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class GetAvailableCountriesControllerTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_it_returns_all_country_codes_relating_to_players(): void
+    {
+        Player::factory()->create(['country_code' => 'bg']);
+        Player::factory()->create(['country_code' => 'ca']);
+        Player::factory()->create(['country_code' => 'kz']);
+
+        $response = $this->get('countries');
+
+        $response->assertOk();
+        $response->assertJson([
+            'bg',
+            'ca',
+            'kz',
+        ]);
+    }
+}
