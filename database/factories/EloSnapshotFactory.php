34d9a43ad324ee38cbe96b3d14ffd7b221d637fe
ryan <ryanennns@gmail.com>
2025-04-23 16:27:25 -0400

    test GetSnapshotForDate

--- database/factories/EloSnapshotFactory.php
+++ database/factories/EloSnapshotFactory.php
@@ -2,22 +2,29 @@
 
 namespace Database\Factories;
 
+use App\Models\EloSnapshot;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\EloSnapshot>
- */
 class EloSnapshotFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
     public function definition(): array
     {
+        $buckets = [];
+        $n = 0;
+        for ($i = 0; $i <= 2300; $i += 100) {
+            $key = "$i-" . ($i + 99);
+            $num = $this->faker->numberBetween(0, 5000);
+
+            $buckets[$key] = $num;
+            $n = $n + $num;
+        }
+
         return [
-            //
+            'date'     => $this->faker->date(),
+            'gamemode' => $this->faker->randomElement(EloSnapshot::GAMEMODES),
+            'buckets'  => json_encode($buckets),
+            'n'        => $n,
         ];
     }
+
 }
