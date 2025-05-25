0428b842d1760fb12c51c37fe4e4820ff975c30b
ryan <ryanennns@gmail.com>
2025-05-25 15:49:37 -0400

    add down method to migration

--- database/migrations/2025_05_25_185411_add_gamemode_specific_ratings_to_players.php
+++ database/migrations/2025_05_25_185411_add_gamemode_specific_ratings_to_players.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     public function up(): void
     {
         Schema::table('players', function (Blueprint $table) {
@@ -18,7 +17,9 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('players', function (Blueprint $table) {
-            //
+            $table->dropColumn('moving_rating');
+            $table->dropColumn('no_move_rating');
+            $table->dropColumn('nmpz_rating');
         });
     }
 };
