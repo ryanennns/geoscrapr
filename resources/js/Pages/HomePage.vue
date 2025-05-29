946445aee3cab826fdf6b881c4f6ba22f9ae04de
Ryan Enns <ryanennns@gmail.com>
2025-05-29 16:14:09 -0400

    refactor phase 2

--- resources/js/Pages/HomePage.vue
+++ resources/js/Pages/HomePage.vue
@@ -76,7 +76,6 @@
         />
         <transition name="fade">
             <RatingHistoryModal
-                v-show="selectedLeaderboardRow !== null"
                 :show-modal="showModal"
                 :leaderboard-row="selectedLeaderboardRow"
                 :rating-history="playerRatingHistory"
@@ -212,39 +211,42 @@ const availableDates = computed<Date[]>(() => {
 });
 
 const selectedDate = ref<Date>(availableDates.value[0]);
-
-const formatDate = (date: Date) => date.toISOString().split("T")[0];
-
+const dateObjectToYmdString = (date: Date) => date.toISOString().split("T")[0];
 const currentSoloRangeSnapshot = computed<Snapshot | undefined>(() =>
-    soloSnapshots.value.find((s) => s.date === formatDate(selectedDate.value)),
+    soloSnapshots.value.find(
+        (s) => s.date === dateObjectToYmdString(selectedDate.value),
+    ),
 );
 const currentTeamRangeSnapshot = computed<Snapshot | undefined>(() =>
-    teamSnapshots.value.find((s) => s.date === formatDate(selectedDate.value)),
+    teamSnapshots.value.find(
+        (s) => s.date === dateObjectToYmdString(selectedDate.value),
+    ),
 );
 const currentSoloPercentileSnapshot = computed<Snapshot | undefined>(() =>
     props.solo_percentile_snapshots.find(
-        (s) => s.date === formatDate(selectedDate.value),
+        (s) => s.date === dateObjectToYmdString(selectedDate.value),
     ),
 );
 const currentTeamPercentileSnapshot = computed<Snapshot | undefined>(() =>
     props.team_percentile_snapshots.find(
-        (s) => s.date === formatDate(selectedDate.value),
+        (s) => s.date === dateObjectToYmdString(selectedDate.value),
     ),
 );
-
 watch(selectedDate, async () => {
     if (
         soloSnapshots.value.find(
-            (s) => s.date === formatDate(selectedDate.value),
+            (s) => s.date === dateObjectToYmdString(selectedDate.value),
         ) &&
         teamSnapshots.value.find(
-            (s) => s.date === formatDate(selectedDate.value),
+            (s) => s.date === dateObjectToYmdString(selectedDate.value),
         )
     ) {
         return;
     }
 
-    const snapshots = await getSnapshotForDate(formatDate(selectedDate.value));
+    const snapshots = await getSnapshotForDate(
+        dateObjectToYmdString(selectedDate.value),
+    );
 
     if (snapshots.error !== undefined) {
         return;
@@ -258,7 +260,11 @@ const { renderRangeChart, renderPercentileChart } = useRatingChart();
 
 const updateCharts = () => {
     if (selectedGraphType.value === "elo_range") {
-        if (!props.range_dates.includes(formatDate(selectedDate.value))) {
+        if (
+            !props.range_dates.includes(
+                dateObjectToYmdString(selectedDate.value),
+            )
+        ) {
             selectedDate.value = new Date(
                 props.range_dates[props.range_dates.length - 1],
             );
@@ -279,7 +285,11 @@ const updateCharts = () => {
     }
 
     if (selectedGraphType.value === "percentile") {
-        if (!props.percentile_dates.includes(formatDate(selectedDate.value))) {
+        if (
+            !props.percentile_dates.includes(
+                dateObjectToYmdString(selectedDate.value),
+            )
+        ) {
             selectedDate.value = new Date(
                 props.percentile_dates[props.percentile_dates.length - 1],
             );
@@ -316,8 +326,6 @@ const initializeCharts = () => {
 };
 
 onMounted(() => {
-    console.log(selectedDate.value)
-
     initializeCharts();
 });
 
