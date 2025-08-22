b9845953f2a5b8e29761436a5d83f35a798e02e5
Ryan Enns <ryanennns@gmail.com>
2025-08-22 09:06:18 -0400

    cleanup

--- app/Models/WorldCupMatch.php
+++ app/Models/WorldCupMatch.php
@@ -8,8 +8,6 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class WorldCupMatch extends Model
 {
-    use HasFactory;
-
     protected $guarded = [];
 
     protected $with = ['playerOne', 'playerTwo', 'winner'];
