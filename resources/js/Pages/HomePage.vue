45a63549483a4207faf6be34b68b3083082501fa
Ryan Enns <ryanennns@gmail.com>
2025-09-11 14:05:40 -0400

    fix modal error II

--- resources/js/Pages/HomePage.vue
+++ resources/js/Pages/HomePage.vue
@@ -148,19 +148,19 @@ const isLoadingHistory = ref<boolean>(false);
 
 const ratingHistoryCache = ref<Record<string, RatingChange[]>>({});
 const onPlayerTeamClick = async (event: { rateable: LeaderboardRow }) => {
-    ,setId(event.rateable.id);
+    setId(event.rateable.id);
 
     const rateable = event.rateable;
     selectedLeaderboardRow.value = rateable;
 
-    ,setTimeout(() => (showModal.value = true), 25);
-    ,if (ratingHistoryCache.value[rateable.id]?.length > 0) {
-        ,playerRatingHistory.value = ratingHistoryCache.value[rateable.id];
+    setTimeout(() => (showModal.value = true), 25);
+    if (ratingHistoryCache.value[rateable.id]?.length > 0) {
+        playerRatingHistory.value = ratingHistoryCache.value[rateable.id];
 
         return;
     }
 
-    ,await getAndSetRateableHistory(rateable);
+    await getAndSetRateableHistory(rateable);
 };
 
 const getAndSetRateableHistory = async (leaderboardRow: LeaderboardRow) => {
@@ -174,7 +174,7 @@ const getAndSetRateableHistory = async (leaderboardRow: LeaderboardRow) => {
 
     ratingHistoryCache.value[leaderboardRow.id] = ratingChanges;
 
-    ,playerRatingHistory.value = ratingChanges.sort(
+    playerRatingHistory.value = ratingChanges.sort(
         (a: RatingChange, b: RatingChange) =>
             new Date(a.created_at).getTime() - new Date(b.created_at).getTime(),
     );
