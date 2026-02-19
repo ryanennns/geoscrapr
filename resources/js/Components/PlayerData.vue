581a22d55555ca89d44818d8e6226051ea30d8ea
Ryan Enns <ryanennns@gmail.com>
2026-02-19 15:26:25 -0500

    longer names

--- resources/js/Components/PlayerData.vue
+++ resources/js/Components/PlayerData.vue
@@ -36,7 +36,7 @@ const allowedNameLength = computed<number>(() => {
         numberOfNullRatings += 1;
     }
 
-    return 13 + numberOfNullRatings * 2;
+    return 25 + numberOfNullRatings * 3;
 });
 
 const croppedName = computed<string>(() => {
